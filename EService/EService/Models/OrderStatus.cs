da28b003e4f7551e407c3d1ed86ed9d5a0852a71
pawel3722 <91870250+pawel3722@users.noreply.github.com>
2024-05-19 23:07:03 +0200

    Statusy

---
+++ EService/EService/Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace EService.Models
+{
+    public static class OrderStatus
+    {
+        public const int StartedProcessing = 0;// -> Przyjęto do realizacji 
+        public const int AssignedManager = 1;  // 1 -> Przypisano menadżera
+        public const int FinishedAnalysis = 2; // 2 -> Ukończono ekspertyzę
+        public const int AssignedActions = 3;  // 3 -> Zlecono wykonanie działań
+        public const int Finished = 4;         // 4 -> Naprawiono
+        public const int Received = 5;         // 5 -> Odebrano
+    }
+}
