3ff4b12b3b16912dfd2cb0b8707e1129fa6f30de
Macio3281 <mm300323@student.polsl.pl>
2024-05-19 23:35:39 +0200

    Merging status and NewEndpoints branches

--- EService/EService/Models/OrderStatus.cs
+++ EService/EService/Models/OrderStatus.cs
@@ -2,11 +2,11 @@
 {
     public static class OrderStatus
     {
-        public const int StartedProcessing = 0;// -> Przyjęto do realizacji 
-        public const int AssignedManager = 1;  // 1 -> Przypisano menadżera
-        public const int FinishedAnalysis = 2; // 2 -> Ukończono ekspertyzę
-        public const int AssignedActions = 3;  // 3 -> Zlecono wykonanie działań
-        public const int Finished = 4;         // 4 -> Naprawiono
-        public const int Received = 5;         // 5 -> Odebrano
+        public const int StartedProcessing = 0; // -> Przyjęto do realizacji 
+        public const int AssignedManager = 1;   // 1 -> Przypisano menadżera
+        public const int FinishedAnalysis = 2;  // 2 -> Ukończono ekspertyzę
+        public const int AssignedActions = 3;   // 3 -> Zlecono wykonanie działań
+        public const int Finished = 4;          // 4 -> Naprawiono
+        public const int Received = 5;          // 5 -> Odebrano
     }
 }
